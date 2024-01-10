68f7b1a946c14433b8382f4234158a05a5fbb13e
itsmibrahim <s.m.ibrahim998@gmail.com>
2024-01-10 19:33:23 +0500

    Ibrahim(1)

---
+++ src/components/main/LoginIB.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const LoginIB = () => {
+  return (
+    <div>
+        <Container >
+            <Col>
+            </Col>
+            <Col>
+            </Col>
+        </Container>
+    </div>
+  )
+}
+
+export default LoginIB
